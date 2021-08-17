c29307abedb8f5490610ff2dbdf3e79258707701
Nicole Linde <Collchen@outlook.de>
2021-08-17 14:41:52 +0200

    added String Concatenation and Check against previous operator

--- src/App.js
+++ src/App.js
@@ -8,9 +8,29 @@ import './App.css';
 
 function App() {
   const [result, setResult] = useState('');
+  const [calc, setCalc] = useState('');
+
+  const operators = ['/', '*', '+', '-'];
 
   function handleClick(event) {
+
+    if (operators.includes(event.target.name) && operators.includes(result.slice(-1))) {
+      return;
+    }
     setResult(result.concat(event.target.name));
+  }
+
+  function clear() {
+    setResult('');
+  }
+
+  function backspace() {
+    setResult(result.slice(0, result.length - 1));
+    console.log(result.length - 1)
+  }
+
+  function calculate() {
+    setResult()
   }
 
   return (
@@ -19,42 +39,29 @@ function App() {
         <input type="text" value={result} />
       </form>
       <div className="keys">
-        <button onClick={handleClick}>C</button>
-        <button onClick={handleClick}>Clear</button>
-        <button name="/" onClick={handleClick}>&divide;</button>
+        <button className="highlight" onClick={clear} id="clear">Clear</button>
+        <button className="highlight" onClick={backspace} id="backspace">C</button>
+        <button className="highlight" name="/" onClick={handleClick}>&divide;</button>
         <button name="7" onClick={handleClick}>7</button>
         <button name="8" onClick={handleClick}>8</button>
         <button name="9" onClick={handleClick}>9</button>
-        <button name="*" onClick={handleClick}>&times;</button>
+        <button className="highlight" name="*" onClick={handleClick}>&times;</button>
         <button name="4" onClick={handleClick}>4</button>
         <button name="5" onClick={handleClick}>5</button>
         <button name="6" onClick={handleClick}>6</button>
-        <button name="-" onClick={handleClick}>&ndash;</button>
+        <button className="highlight" name="-" onClick={handleClick}>&ndash;</button>
         <button name="1" onClick={handleClick}>1</button>
         <button name="2" onClick={handleClick}>2</button>
         <button name="3" onClick={handleClick}>3</button>
-        <button name="+" onClick={handleClick}>+</button>
+        <button className="highlight" name="+" onClick={handleClick}>+</button>
         <button name="0" onClick={handleClick}>0</button>
         <button name="." onClick={handleClick}>.</button>
-        <button onClick={handleClick}>=</button>
+        <button className="highlight" onClick={handleClick} id="result">=</button>
 
 
       </div>
     </div >
 
-
-    /* { <Headline>Calculator</Headline>
-    <Result>{counter}</Result>
-    <Button onButtonClick={() => calculate(Math.floor(Math.random() * (-10)))}>-random</Button>
-    <Button onButtonClick={() => calculate(-10)}>-10</Button>
-    <Button onButtonClick={() => calculate(-1)}>-1</Button>
-    <Button onButtonClick={() => calculate(1)}>+1</Button>
-    <Button onButtonClick={() => calculate(10)}>+10</Button>
-    <Button onButtonClick={() => calculate(Math.floor(Math.random() * 10))}>+ random</Button>
-    <Resetbutton clickToReset={() => reset()}>Reset</Resetbutton>
- 
-);
-} */
   )
 }
 export default App;
